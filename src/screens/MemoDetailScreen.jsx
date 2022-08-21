418b2c812995ee10add60c17ee00f8250d7e6868
raiki1024 <events1822@gmail.com>
2022-08-22 00:14:04 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -46,11 +46,12 @@ export default function MemoDetailScreen(props) {
       <CircleButton
         style={{ top: 60, bottom: 'auto' }}
         name="pencil"
-        onPress={() => { navigation.navigate('MemoEdit'); }}
+        onPress={() => { navigation.navigate('MemoEdit', { id: memo.id, bodyText: memo.bodyText }); }}
       />
     </View>
   );
 }
+
 MemoDetailScreen.propTypes = {
   route: shape({
     params: shape({ id: string }),
